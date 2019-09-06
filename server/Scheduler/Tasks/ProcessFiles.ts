dec5ed6f0ff2872697dde89284d3fd689caf9b9f
Abhishek Prakash <prakashabhishek6262@gmail.com>
2019-09-07 02:55:06 +0530

    Setup process files scheduler

--- server/Scheduler/Tasks/ProcessFiles.ts
+++ server/Scheduler/Tasks/ProcessFiles.ts
@@ -1,8 +1,27 @@
+import FileModel from '../../Database/Models/FileModel'
 import ITask from './ITask'
 
 class ProcessFiles implements ITask {
-  handle() {
-    // console.log('Processing files...')
+  async handle() {
+    const processingFiles = await FileModel.find({ status: 'processing' })
+
+    // Terminate if some files are already processing to avoid processing
+    // too many files at once and overloading the server.
+    if (processingFiles.length > 0) {
+      return
+    }
+
+    const pendingFiles = await FileModel.find({ status: 'pending' }).limit(2)
+
+    if (pendingFiles.length < 1) {
+      return
+    }
+
+    pendingFiles.forEach(file => {
+      file.updateOne({ status: 'processing' }).exec()
+
+      // TODO: Start a thread to handle the download process.
+    })
   }
 }
 
