516c566420c75f29a38ab740d79fe9c94c3afaf8
Abhishek Prakash <prakashabhishek6262@gmail.com>
2019-09-07 20:02:03 +0530

    Requeue failed files

--- server/Scheduler/Tasks/ProcessFiles.ts
+++ server/Scheduler/Tasks/ProcessFiles.ts
@@ -9,15 +9,29 @@ class ProcessFiles extends Task {
       Source.downloadFile(file.sourceLink, '/static/downloads', async (downloadPercent: number) => {
         if (downloadPercent === 0) {
           await file.updateOne({ status: 'processing' }).exec()
-        }
+        } else if (downloadPercent === 50) {
+          // TODO: Mail user about the half download.
+        } else if (downloadPercent === 100) {
+          await file.updateOne({ status: 'completed' }).exec()
 
-        // 
+          // TODO: Mail user about the completion of the download.
+        }
       })
     } catch (err) {
       await file.updateOne({ status: 'failed' }).exec()
 
-      // TODO: Log the failure.
+      // TODO: Mail user about the failed download.
     }
+  }
+
+  private async requeueFailedFiles() {
+    // TODO: Add limit or attempts to failed files so server does not
+    // requeue them over and over again for the entire life.
+    const failedFiles = await FileModel.find({ status: 'failed' })
+
+    failedFiles.forEach(async (file) => {
+      await file.updateOne({ status: 'pending' }).exec()
+    })
   }
 
   async handle() {
@@ -31,7 +45,6 @@ class ProcessFiles extends Task {
       return
     }
 
-    // TODO: Set failed files to pending.
     const pendingFiles = await FileModel.find({ status: 'pending' }).limit(2)
 
     if (pendingFiles.length < 1) {
@@ -41,6 +54,10 @@ class ProcessFiles extends Task {
     }
 
     pendingFiles.forEach(this.processFile)
+
+    // Add failed files to queue at the end so newly added files are given
+    // priority over them.
+    this.requeueFailedFiles()
   }
 }
 
