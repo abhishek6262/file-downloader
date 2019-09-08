37ede0cbb7165fff3400a548e351eee02e7ae85e
Abhishek Prakash <prakashabhishek6262@gmail.com>
2019-09-08 14:29:47 +0530

    TODO: Clean disk space

--- server/Scheduler/Scheduler.ts
+++ server/Scheduler/Scheduler.ts
@@ -1,9 +1,11 @@
 import Engine from './Engine'
 import Task from './Tasks/Task'
+import CleanDiskSpace from './Tasks/CleanDiskSpace'
 import ProcessFiles from './Tasks/ProcessFiles'
 
 class Scheduler extends Engine {
   static tasks: Task[] = [
+    new CleanDiskSpace,
     new ProcessFiles,
   ]
 }
---
+++ server/Scheduler/Tasks/CleanDiskSpace.ts
@@ -0,0 +1,9 @@
+import Task from './Task'
+
+class CleanDiskSpace extends Task {
+  async handle() {
+    // TODO: Handle cleaning disk space by removing old downloads.
+  }
+}
+
+export default CleanDiskSpace
