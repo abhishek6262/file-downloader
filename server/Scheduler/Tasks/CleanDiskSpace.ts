f157cc53f94871b7b7744114ee3a849c68152ad9
Abhishek Prakash <prakashabhishek6262@gmail.com>
2019-09-11 00:12:01 +0530

    Remove unnecessary disk space

--- server/Scheduler/Tasks/CleanDiskSpace.ts
+++ server/Scheduler/Tasks/CleanDiskSpace.ts
@@ -1,11 +1,29 @@
+import Fs from 'fs'
+import Path from 'path'
 import Task from './Task'
+import FileModel from '../../Database/Models/FileModel'
 
 class CleanDiskSpace extends Task {
-  handle() {
-    // TODO: Handle cleaning disk space by removing old downloads.
-    console.log('Cleaning disk space ...')
+  async handle() {
+    const today = new Date
 
-    this.sleep(600)
+    const files = await FileModel.find({
+      status: { $ne: 'expired' },
+      updatedAt: { $lte: today.setMonth(today.getMonth() - 1) }
+    })
+
+    for (const file of files) {
+      const filePath = Path.resolve(__dirname, './../../../' + file.downloadLink)
+
+      if (Fs.existsSync(filePath)) {
+        Fs.unlinkSync(filePath)
+      }
+
+      await file.updateOne({ status: 'expired', updatedAt: new Date })
+    }
+
+    // Query for disk space in every 2 hours.
+    this.sleep(7200)
   }
 }
 
